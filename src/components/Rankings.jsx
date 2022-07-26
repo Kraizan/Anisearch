3f7dca63219edc7ab45f264072d9917679bdc44c
Vanshaj Bhatnagar <63310123+Kraizan@users.noreply.github.com>
2022-07-26 14:49:25 +0530

    Loading screen completed.
    Created a hashloader using react-spinners package.

--- src/components/Rankings.jsx
+++ src/components/Rankings.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios"
 import Pagination from '@mui/material/Pagination';
+import HashLoader from "react-spinners/HashLoader";
 import Card from "./Card";
 
 function Rankings(props){
@@ -10,14 +11,17 @@ function Rankings(props){
   const [fetchData, setFetchData] = useState({});
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(function effectFunction() {
     async function fetchData(currentPage) {
+      setLoading(true);
       const response = await axios.get(
         url + (props.popularity ? "&" : "?") + "page=" + currentPage);
       const res = await response.data;
       setFetchData(res.data);
       setTotalPages(res.pagination.last_visible_page);
+      setLoading(false);
     }
     fetchData(currentPage);
   }, [currentPage]);
@@ -43,24 +47,42 @@ function Rankings(props){
   }
 
   return (
-    <div className="ranking-div">
-      {animeData.map((anime) => {
-           return (
-             <Card
-               key= {anime.id}
-               id= {anime.id}
-               name= {anime.name}
-               type= {anime.type}
-               rank= {anime.rank}
-               popularity= {anime.popularity}
-               score= {anime.score}
-               year= {anime.year}
-               episodes= {anime.episodes}
-               url= {anime.url}
-             />
-           );
-      })}
-      <Pagination count={totalPages} className="pagination" page={currentPage} onChange={handleChange} />
+    <div>
+      {
+        loading
+        ? <HashLoader
+          color="#2db3ac"
+          className="loader"
+          cssOverride={{
+            position: 'fixed',
+            top: '46%',
+            left: '46%'
+          }}
+          size={120}
+          speedMultiplier={0.8}
+        />
+        : <div>
+          <div className="ranking-div">
+            {animeData.map((anime) => {
+                 return (
+                   <Card
+                     key= {anime.id}
+                     id= {anime.id}
+                     name= {anime.name}
+                     type= {anime.type}
+                     rank= {anime.rank}
+                     popularity= {anime.popularity}
+                     score= {anime.score}
+                     year= {anime.year}
+                     episodes= {anime.episodes}
+                     url= {anime.url}
+                   />
+                 );
+            })}
+            <Pagination count={totalPages} className="pagination" page={currentPage} onChange={handleChange} />
+          </div>
+        </div>
+      }
     </div>
   )
 }
